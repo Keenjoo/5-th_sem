e4572c4d22c7c234a170e0ce1f3279e4b365f0f7
unknown <keenjosapkota111@gmail.com>
2024-06-06 08:20:31 +0545

    FIRST COMMIT

---
+++ stringMethod.js
@@ -0,0 +1,23 @@
+var text="I am currently in BscCSIT 5TH sem";
+//alert(text);
+console.log (text);
+
+//print the length of given string
+console.log("this is length of text:",text.length);
+
+//print ihe character oa index 5 of the given string
+console.log("the character of index 5 is:",text.charAt(5));
+
+//print ihe character oa index 5 of the given string
+console.log("the character of index 5 is:",text.charCodeAt(5));
+
+
+//print ihe character oa index 5 of the given string
+console.log("the character of index 5 using string[]:",text[5]);
+
+// print and write it into the document ,text between 3 and 10 index
+console.log("the text between 3 and 10 index is",text.slice(3.10));
+
+document.write("the text between 3 and 10 index is",text.slice(3.10));
+
+
